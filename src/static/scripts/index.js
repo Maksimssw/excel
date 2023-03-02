e313124ebee54dd50876b20ab0723f19672e7632
Maks <kiva_hill@mail.ru>
2023-03-02 22:56:13 +0300

    feat(layout): Add list tables

--- src/static/scripts/index.js
+++ src/static/scripts/index.js
@@ -1,9 +1,26 @@
-import '../styles/global.css'
+{
+  // Functionality in the form of a list and in the form of a grid
+  const list = document.querySelector('.list')
+  const grid = document.querySelector('.grid')
+  const buttons = document.querySelectorAll('.dashboard__button')
 
-async function start() {
-  return Promise.resolve('Start');
+  const toggleClassButton = () => {
+    if (localStorage.getItem('dasboard-button-active') === 'true') {
+      list.classList.remove('dashboard__button_active')
+      grid.classList.add('dashboard__button_active')
+    } else {
+      list.classList.add('dashboard__button_active')
+      grid.classList.remove('dashboard__button_active')
+    }
+  }
+
+  toggleClassButton()
+
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      localStorage.setItem('dasboard-button-active', grid.classList.toggle('dashboard__button_active'))
+
+      toggleClassButton()
+    })
+  })
 }
-
-console.log('start');
-
-start().then(console.log);
